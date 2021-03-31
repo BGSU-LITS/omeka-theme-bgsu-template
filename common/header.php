5a6fec070603a574cb81ee34aebd839e2aeb2818
John Kloor <john@kloor.org>
2021-03-31 11:33:46 -0400

    Add CSS for forms.

--- common/header.php
+++ common/header.php
@@ -79,6 +79,8 @@ get_view()->headScript()
     ->prependFile(BGSU_TEMPLATE . 'template.js')
     ->prependFile(BGSU_TEMPLATE . 'tippy.js');
 
+queue_css_file('forms');
+
 echo head_css() . PHP_EOL . head_js(false) . PHP_EOL;
 echo '</head>' . PHP_EOL;
 
